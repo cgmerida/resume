72df1774b25c886275864e3df59e404899f1e933
Gerardo Merida <cgemerida@gmail.com>
2022-04-10 10:38:30 -0600

    feat(resume): add resume page with containing elements

---
+++ src/Resume.js
@@ -0,0 +1,12 @@
+import './Resume.css';
+import Header from './Components/Header/Header';
+import Body from './Components/Body/Body';
+
+const Resume = () => (
+  <div className="page box">
+    <Header />
+    <Body />
+  </div>
+);
+
+export default Resume;
